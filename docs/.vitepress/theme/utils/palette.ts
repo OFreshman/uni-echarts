4ab96ec6e9be124ec8b08205a85af1c6cdf8067b
xiaohe0601 <xiaohe0601@outlook.com>
2026-03-31 15:05:14 +0800

    chore: switch `color` to `colord`

--- docs/.vitepress/theme/utils/palette.ts
+++ docs/.vitepress/theme/utils/palette.ts
@@ -1,22 +1,22 @@
-import Color from "color";
+import { colord } from "colord";
 
 export function createPalette(value: string) {
-  const color = new Color(value);
+  const color = colord(value);
 
-  const [h, s, l] = color.hsl().array();
+  const { h, s, l } = color.toHsl();
 
   return {
     light: {
-      color1: color.hex().toLowerCase(),
-      color2: Color.hsl(h - 1.22, s - 9.6, l + 9.21).hex().toLowerCase(),
-      color3: Color.hsl(h - 2.37, s - 12.18, l + 13.13).hex().toLowerCase(),
-      soft: Color.hsl(h + 6.71, s + 17.18, l + 10.98).alpha(0.14).rgb().string()
+      color1: color.toHex(),
+      color2: colord({ h: h - 1.22, s: s - 9.6, l: l + 9.21 }).toHex(),
+      color3: colord({ h: h - 2.37, s: s - 12.18, l: l + 13.13 }).toHex(),
+      soft: colord({ h: h + 6.71, s: s + 17.18, l: l + 10.98 }).alpha(0.14).toRgbString()
     },
     dark: {
-      color1: Color.hsl(h - 2.42, s - 1.79, l + 25.49).hex().toLowerCase(),
-      color2: Color.hsl(h - 2.37, s - 12.18, l + 13.13).hex().toLowerCase(),
-      color3: Color.hsl(h - 1.76, s - 14.29, l + 5.49).hex().toLowerCase(),
-      soft: Color.hsl(h + 6.71, s + 17.18, l + 10.98).alpha(0.16).rgb().string()
+      color1: colord({ h: h - 2.42, s: s - 1.79, l: l + 25.49 }).toHex(),
+      color2: colord({ h: h - 2.37, s: s - 12.18, l: l + 13.13 }).toHex(),
+      color3: colord({ h: h - 1.76, s: s - 14.29, l: l + 5.49 }).toHex(),
+      soft: colord({ h: h + 6.71, s: s + 17.18, l: l + 10.98 }).alpha(0.16).toRgbString()
     }
   };
 }
